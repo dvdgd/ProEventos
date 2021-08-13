ad82ab4ea9dcea1f798c4062325926a14525c9b9
David Dutra <dvd.gd762@gmail.com>
2021-08-13 00:44:19 -0300

    Consumindo API no front-end

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -5,11 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
-  declarations: [AppComponent, EventosComponent, PalestrantesComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    EventosComponent,
+    PalestrantesComponent
+  ],
+
+  imports:
+  [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+
+  providers: [
+
+  ],
+
+  bootstrap: [
+    AppComponent
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,22 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any = [
-    {
-      Tema: 'Angular 11',
-      Local: 'Rio de Janeiro',
-    },
-    {
-      Tema: '.NET',
-      Local: 'São Paulo',
-    },
-    {
-      Tema: 'Angular e suas novidades',
-      Local: 'Belo Horizonte',
-    },
-  ];
+  public eventos: any;
 
-  constructor() {}
+  constructor(private http : HttpClient ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+
+    this.http.get('https://localhost:5001/api/evento').subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    );
+  }
 }
