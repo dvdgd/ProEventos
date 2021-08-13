2347817eda9c4afcde5eb4e8752cab6542929794
David Dutra <dvd.gd762@gmail.com>
2021-08-13 01:08:23 -0300

    Alterandoo rota de (api/evento) para (api/eventos)

--- Back/src/ProEvento.API/Controllers/EventoController.cs
+++ Back/src/ProEvento.API/Controllers/EventoController.cs
@@ -11,11 +11,11 @@ namespace ProEvento.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class EventoController : ControllerBase
+    public class EventosController : ControllerBase
     {
         private readonly DataContext _context;
 
-        public EventoController(DataContext context)
+        public EventosController(DataContext context)
         {
             this._context = context;
         }
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,17 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
   public eventos: any;
 
-  constructor(private http : HttpClient ) {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getEventos();
   }
 
   public getEventos(): void {
-
-    this.http.get('https://localhost:5001/api/evento').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      (response) => (this.eventos = response),
+      (error) => console.log(error)
     );
   }
 }
