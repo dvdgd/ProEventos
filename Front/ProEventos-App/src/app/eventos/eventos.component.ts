1dc4bc4e4a34b15d09be554e0a4281f193bdffaa
David Dutra <dvd.gd762@gmail.com>
2021-09-03 17:42:21 -0300

    Adiconando imagens (eventos component)

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,12 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any = [];
+
+  larugraImg = 150;
+  margemImg = 2;
+  exibirImg = true;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getEventos();
+  }
+
+  public alterarImagens(): void {
+    this.exibirImg = !this.exibirImg;
   }
 
   public getEventos(): void {
