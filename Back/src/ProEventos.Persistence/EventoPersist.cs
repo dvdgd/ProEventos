5040b3bc2ff7de8740a85593cbd0ef1c7d829a95
David Dutra <dvd.gd762@gmail.com>
2021-09-29 18:30:50 -0300

    ProEventos.Persistence: Implementando interfaces

---
+++ Back/src/ProEventos.Persistence/EventoPersist.cs
@@ -0,0 +1,72 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain;
+using ProEventos.Persistence.Context;
+using ProEventos.Persistence.Interfaces;
+
+namespace ProEventos.Persistence
+{
+    public class EventoPersist : IEventoPersist
+    {
+        private readonly ProEventosContext _context;
+        public EventoPersist(ProEventosContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false)
+        {
+            IQueryable<Evento> query = _context.Eventos
+                .Include(evento => evento.Lote)
+                .Include(evento => evento.RedesSociais);
+
+            if (includePalestrantes)
+            {
+                query = query
+                    .Include(evento => evento.PalestranteEventos)
+                    .ThenInclude(palestranteEventos => palestranteEventos.Palestrante);
+            }
+
+            query = query.OrderBy(e => e.Id);
+
+            return await query.ToArrayAsync();
+        }
+
+        public async Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes = false)
+        {
+            IQueryable<Evento> query = _context.Eventos.Where(evento => evento.Tema.ToUpper().Contains(tema.ToUpper()))
+                .Include(evento => evento.Lote)
+                .Include(evento => evento.RedesSociais);
+
+            if (includePalestrantes)
+            {
+                query = query
+                    .Include(evento => evento.PalestranteEventos)
+                    .ThenInclude(palestranteEvento => palestranteEvento.Palestrante);
+            }
+
+            query = query.OrderBy(evento => evento.Id);
+
+            return await query.ToArrayAsync();
+        }
+
+        public async Task<Evento> GetEventoByIdAsync(int eventoId, bool includePalestrantes = false)
+        {
+            IQueryable<Evento> query = _context.Eventos.Where(evento => evento.Id == eventoId)
+                .Include(evento => evento.RedesSociais);
+
+            if (includePalestrantes)
+            {
+                query = query
+                    .Include(evento => evento.PalestranteEventos)
+                    .ThenInclude(palestranteEventos => palestranteEventos.Palestrante);
+            }
+
+            query = query.OrderBy(evento => evento.Id);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+    }
+}
