9420bf4b2b079f5d2aee47db8127ecfd16e55fe7
David Dutra <dvd.gd762@gmail.com>
2021-09-29 17:55:12 -0300

    ProEventos.Persistence: Interfaces

---
+++ Back/src/ProEventos.Persistence/Interfaces/IEventoPersist.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence.Interfaces
+{
+    public interface IEventoPersist
+    {
+        Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes);
+        Task<Evento[]> GetAllEventosAsync(bool includePalestrantes);
+        Task<Evento> GetEventoByIdAsync(int eventoId, bool includePalestrantes);
+
+    }
+}
