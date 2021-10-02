ef238c54a4e301f9c42840bf70bd080a9b15b4e2
David Dutra <71572000+dvdgd@users.noreply.github.com>
2021-10-02 19:22:40 -0300

    ProEventos.Domain: nova propriedade na Entidade RedeSocial

--- Back/src/ProEventos.Domain/RedeSocial.cs
+++ Back/src/ProEventos.Domain/RedeSocial.cs
@@ -6,6 +6,7 @@ namespace ProEventos.Domain
         public string Nome { get; set; }
         public string URL { get; set; }
         public int? EventoId { get; set; }
+        public Evento Evento { get; set; }
         public int? PalestranteId { get; set; }
         public Palestrante Palestrante { get; set; }
     }
